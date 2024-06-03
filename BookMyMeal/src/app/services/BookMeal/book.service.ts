f66219243153a8c82061422ec763a3e57455d98a
Raghav <raghavmistry123@gmail.com>
2024-06-03 16:31:32 +0530

    removed all unwanted code

--- BookMyMeal/src/app/services/BookMeal/book.service.ts
+++ BookMyMeal/src/app/services/BookMeal/book.service.ts
@@ -1,22 +1,3 @@
-// import { Injectable } from '@angular/core';
-// import { Observable } from 'rxjs';
-// import { HttpClient } from '@angular/common/http';
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class BookService {
-
-//   private baseUrl = 'http://localhost:8080/api/bookings'; // Adjust the base URL as needed
-
-//   constructor(private http: HttpClient) {}
-
-//   BookingBulk(bookingData: any): Observable<any> {
-//     return this.http.post(`${this.baseUrl}/multiple`, bookingData);
-//   }
-
-
-// }
 
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
