b88f7260aa0ef54251b3c86abebb383215b0b3fc
Raghav <raghavmistry123@gmail.com>
2024-06-03 13:49:39 +0530

    notification connected

--- BookMyMeal/src/app/services/notification.service.ts
+++ BookMyMeal/src/app/services/notification.service.ts
@@ -1,8 +1,45 @@
+// import { Injectable } from '@angular/core';
+// import { BehaviorSubject } from 'rxjs';
+
+// interface Notification {
+//   date: Date;
+//   message: string;
+// }
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class NotificationService {
+//   private notifications: Notification[] = [];
+//   private notificationCount = new BehaviorSubject<number>(0);
+
+//   notificationCount$ = this.notificationCount.asObservable();
+
+//   constructor() {
+//     this.initializeNotifications();
+//   }
+
+//   private initializeNotifications(): void {
+//     for (let i = 1; i <= 20; i++) {
+//       this.notifications.push({
+//         date: new Date(),
+//         message: `This is notification message number ${i}`
+//       });
+//     }
+//     this.notificationCount.next(this.notifications.length);
+//   }
+
+//   getNotifications(): Notification[] {
+//     return this.notifications;
+//   }
+// }
+
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 interface Notification {
-  date: Date;
+  dateTime: Date;
   message: string;
 }
 
@@ -10,26 +47,11 @@ interface Notification {
   providedIn: 'root'
 })
 export class NotificationService {
-  private notifications: Notification[] = [];
-  private notificationCount = new BehaviorSubject<number>(0);
+  private apiUrl = 'http://localhost:8080/api/notifications'; // Adjust the URL according to your backend
 
-  notificationCount$ = this.notificationCount.asObservable();
+  constructor(private http: HttpClient) {}
 
-  constructor() {
-    this.initializeNotifications();
-  }
-
-  private initializeNotifications(): void {
-    for (let i = 1; i <= 20; i++) {
-      this.notifications.push({
-        date: new Date(),
-        message: `This is notification message number ${i}`
-      });
-    }
-    this.notificationCount.next(this.notifications.length);
-  }
-
-  getNotifications(): Notification[] {
-    return this.notifications;
+  getNotifications(): Observable<Notification[]> {
+    return this.http.get<Notification[]>(this.apiUrl);
   }
 }
